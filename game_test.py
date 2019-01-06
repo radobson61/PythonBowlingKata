9100c3f838ece5b0a0f6009cbfefcec230189ed8
Bob Dobson <bobdobson@MacBookAir.local>
2019-01-06 15:32:07 -0600

    Resolve second squiggle

--- game_test.py
+++ game_test.py
@@ -5,4 +5,7 @@ class Game_Tests(unittest.TestCase):
 
     def test_when_all_rolls_are_0_score_is_0(self):
         target = Game()
+        for roll in range(20):
+            target.roll(0)
+            
         
