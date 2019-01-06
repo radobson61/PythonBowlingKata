c0c395719f0000fb4969f08c26f9b0b5c12ef094
Bob Dobson <bobdobson@MacBookAir.local>
2019-01-06 15:44:00 -0600

    Second test passes (only changed production code)

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
 class Game():
     
+    def __init__(self, *args, **kwargs):
+        self._score = 0
+    
     def roll(self, pins):
-        ...
+        self._score += pins
     
     def score(self):
-        return 0 
+        return self._score 
