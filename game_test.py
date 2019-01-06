fd918ef215eb27a6e6f0dfe81c73986e9271a7c0
Bob Dobson <bobdobson@MacBookAir.local>
2019-01-06 16:04:28 -0600

    Second Test Refactor: for loop

--- game_test.py
+++ game_test.py
@@ -14,14 +14,16 @@ class Game_Tests(unittest.TestCase):
         self._target = None
 
     def test_when_all_rolls_are_0_score_is_0(self):
-        for roll in range(20):
-            self._target.roll(0)
+        self._rollMany(20, 0)
         self.assertEqual(0, self._target.score())
 
     def test_when_all_rolls_are_1_score_is_1(self):
-        for roll in range(20):
-            self._target.roll(1)
+        self._rollMany(20, 1)
         self.assertEqual(20, self._target.score())
+
+    def _rollMany(self, rolls, pins):
+        for roll in range(rolls):
+            self._target.roll(pins)
 
 
         
