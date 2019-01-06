990dbe75d65fe09c582c1a8660538d60d1e315dd
Bob Dobson <bobdobson@MacBookAir.local>
2019-01-06 15:59:13 -0600

    Second Test Refactor: Game() initialization

--- game_test.py
+++ game_test.py
@@ -3,17 +3,25 @@ from game import Game
 
 class Game_Tests(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        self._target = Game()
+        super().__init__(*args, **kwargs)
+
+    def setUp(self):
+        self._target = Game()
+
+    def tearDown(self):
+        self._target = None
+
     def test_when_all_rolls_are_0_score_is_0(self):
-        target = Game()
         for roll in range(20):
-            target.roll(0)
-        self.assertEqual(0, target.score())
+            self._target.roll(0)
+        self.assertEqual(0, self._target.score())
 
     def test_when_all_rolls_are_1_score_is_1(self):
-        target = Game()
         for roll in range(20):
-            target.roll(1)
-        self.assertEqual(20, target.score())
+            self._target.roll(1)
+        self.assertEqual(20, self._target.score())
 
 
         
