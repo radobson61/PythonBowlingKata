8896de432e5c4dc1b2622c39b161dcbc8144a855
Bob Dobson <bobdobson@MacBookAir.local>
2019-01-06 15:41:01 -0600

    Second test fails

--- game_test.py
+++ game_test.py
@@ -9,5 +9,11 @@ class Game_Tests(unittest.TestCase):
             target.roll(0)
         self.assertEqual(0, target.score())
 
-        
+    def test_when_all_rolls_are_1_score_is_1(self):
+        target = Game()
+        for roll in range(20):
+            target.roll(1)
+        self.assertEqual(20, target.score())
+
+
         
